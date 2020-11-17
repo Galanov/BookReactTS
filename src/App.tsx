bcc240b04fd57e876febf844f7786c3881b16617
Nikita Galanov <galanov888@gmail.com>
2020-11-17 10:50:04 +0300

    начало примера

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+//import './App.css';
+import {Header} from './Header';
+
+//import { css, jsx } from '@emotion/core';
+
+const App: React.FC = () =>{
+    return(
+        <div className="App">
+            <Header />
+        </div>
+    );
+};
+
+export default App;

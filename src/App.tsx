1539505be73c8f050f24b0beec5447c584610ac5
Nikita Galanov <galanov888@gmail.com>
2020-11-18 00:30:06 +0300

    добавлены стили

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
-import './App.css';
 import {Header} from './Header';
-import {HomePage} from './HomePage'
+import {HomePage} from './HomePage';
 
-//import { css, jsx } from '@emotion/core';
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import {css, jsx} from '@emotion/core';
+import {fontFamily, fontSize, gray2} from './Styles'
 
 const App: React.FC = () =>{
     return(
-        <div className="App">
+        <div 
+            css={
+            css`
+            font-family: ${fontFamily};
+            font-size: ${fontSize};
+            color: ${gray2};
+        `}>
             <Header />
             <HomePage />
         </div>
--- src/Icons.tsx
+++ src/Icons.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
 import user from './user.svg';
 
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core';
+
+
 export const UserIcon = () => 
-    <img src={user} alt="User" width="12px"/>;
+    <img src={user} alt="User" width="12px"
+        css={css`
+            width: 12px;
+            opacity: 0.6;
+        `}
+    />;

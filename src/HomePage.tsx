28356622d5e395e56227fd9976677f14da79017d
Nikita Galanov <galanov888@gmail.com>
2020-11-18 22:47:20 +0300

    доработка страницы

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,10 +1,35 @@
-import React from 'react';
+//import React from 'react';
+
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core';
+
+import { PrimaryButton } from "./Styles";
+import { QuestionList } from "./QuestionList";
+import { getUnansweredQuestions } from "./QuestionsDate";
+import { Page } from "./Page";
+import { PageTitle } from "./PageTitle";
 
 export const HomePage = () => (
-    <div>
-        <div>
-            <h2>Unanswered Questions</h2>
-            <button>Ask a question</button>
+    <Page>
+        <div
+            css={css`
+                display: flex;
+                align-items: center;
+                justify-content: space-between;
+            `}
+        >
+            <h2
+                css={css`
+                    display: flex;
+                    align-items: center;
+                    justify-content: space-between;
+                `}
+            >
+                <PageTitle>Unanswered Questions</PageTitle>
+            </h2>
+            <PrimaryButton>Ask a question</PrimaryButton>
         </div>
-    </div>
+        <QuestionList data={getUnansweredQuestions()}/>
+    </Page>
 );
